2ed62c029cf42e6a698b065f80629f45d1128b69
Süleyman Kayabaşı <82765835+suleymankayabasi@users.noreply.github.com>
2022-01-12 22:08:59 +0300

    Revert "Feedback"

--- src/main/java/com/example/homework3/mongodb/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.homework3.mongodb.repository;
-
-import com.example.homework3.mongodb.entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface UserRepository extends MongoRepository<User,String> {
-
-}
